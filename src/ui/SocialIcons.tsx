3322df30bf2a56f444049e3fe6521f02eb75448c
Fares Mohamed <faresmohamed698@gmail.com>
2024-09-19 04:44:37 +0300

    update links

--- src/ui/SocialIcons.tsx
+++ src/ui/SocialIcons.tsx
@@ -7,21 +7,22 @@ function SocialIcons() {
   return (
     <div className="flex flex-col items-center mx-auto space-y-10 text-center lg:flex-row lg:space-y-0 lg:w-1/2 lg:gap-6 text-yellow-300/90">
       <Link
-        to="https://x.com/Faresmo221"
+        to="https://github.com/FaresMo2"
         target="_blank"
         className="flex items-center justify-center transition duration-300 rounded-md cursor-pointer lg:w-10 lg:bg-zinc-900 lg:aspect-square hover:bg-zinc-700"
       >
         <FaGithub />
       </Link>
+      https://x.com/Faresmo221
       <Link
-        to="https://github.com/FaresMo2"
+        to="https://www.linkedin.com/in/fares-mohamed-576968267/"
         target="_blank"
         className="flex items-center justify-center transition duration-300 rounded-md cursor-pointer lg:w-10 lg:bg-zinc-900 lg:aspect-square hover:bg-zinc-700"
       >
         <FaLinkedin />
       </Link>
       <Link
-        to="https://www.linkedin.com/in/fares-mohamed-576968267/"
+        to="https://x.com/Faresmo221"
         target="_blank"
         className="flex items-center justify-center transition duration-300 rounded-md cursor-pointer lg:w-10 lg:bg-zinc-900 lg:aspect-square hover:bg-zinc-700"
       >
